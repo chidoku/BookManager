dad04716fa58ffccde990412810c85d9c50e03e6
chidoku <75946418+chidoku@users.noreply.github.com>
2021-06-24 14:47:59 +0700

    Initial commit

---
+++ BookManager/Controllers/BookController.cs
@@ -0,0 +1,113 @@
+﻿using BookManager.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BookManager.Controllers
+{
+    public class BookController : Controller
+    {
+        // GET: Book
+        public ActionResult ListBook()
+        {
+            BookManagerContext context = new BookManagerContext();
+            var listBook = context.Books.ToList();
+            return View(listBook);
+        }
+        [Authorize]
+        public ActionResult Buy(int id)
+        {
+            BookManagerContext context = new BookManagerContext();
+            Book book = context.Books.SingleOrDefault(p => p.ID == id);
+            if (book == null)
+            {
+                return HttpNotFound();
+            }
+            return View(book);
+        }
+        public ActionResult Details(int id)
+        {
+            using (BookManagerContext context = new BookManagerContext())
+            {
+                return View(context.Books.Where(x => x.ID == id).FirstOrDefault());
+            }
+        }
+        [Authorize]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Book book)
+        {
+            BookManagerContext context = new BookManagerContext();
+            List<Book> listBook = context.Books.ToList();
+            if (ModelState.IsValid)
+            {
+                context.Books.Add(book);
+                context.SaveChanges();
+
+                return RedirectToAction("listbook");
+            }
+            else
+            {
+                return View(book);
+            }
+        }
+        public ActionResult Edit(int id)
+        {
+            using (BookManagerContext context = new BookManagerContext())
+            {
+                return View(context.Books.Where(x => x.ID == id).FirstOrDefault());
+            }
+        }
+        [Authorize]
+        [HttpPost]
+        public ActionResult Edit(int id,Book book)
+        {
+            try
+            {
+                using (BookManagerContext context = new BookManagerContext())
+                {
+                    context.Entry(book).State = EntityState.Modified;
+                    context.SaveChanges();
+                }
+                return RedirectToAction("ListBook");
+            }
+            catch
+            {
+                return View();
+            }
+
+        }
+        public ActionResult Delete(int id)
+        {
+            using (BookManagerContext context = new BookManagerContext())
+            {
+                return View(context.Books.Where(x => x.ID == id).FirstOrDefault());
+            }
+        }
+        [Authorize]
+        [HttpPost]
+        public ActionResult Delete(int id,Book book)
+        {
+            using (BookManagerContext context = new BookManagerContext()) {
+                Book dbDelete = context.Books.FirstOrDefault(p => p.ID == id);
+                if (dbDelete != null)
+                {
+                    context.Books.Remove(book);
+                    context.SaveChanges();
+                }
+                return RedirectToAction("ListBook");
+            }
+               
+        }
+
+    }
+   
+}
---
+++ BookManager/Models/Book.cs
@@ -0,0 +1,34 @@
+﻿namespace BookManager.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("Book")]
+    public partial class Book
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int ID { get; set; }
+
+        [Required(ErrorMessage = "không được để trống")]
+        [StringLength(100, ErrorMessage = "không được quá 100 ký tự")]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "không được để trống")]
+        [StringLength(255)]
+        public string Description { get; set; }
+
+        [Required(ErrorMessage = "không được để trống")]
+        [StringLength(30, ErrorMessage = "không được quá 30 ký tự")]
+        public string Author { get; set; }
+
+        [Required(ErrorMessage = "không được để trống")]
+        [StringLength(255)]
+        public string Images { get; set; }
+        [Required(ErrorMessage = "không được để trống")]
+        [Range(1000, 1000000, ErrorMessage = "Giá sách từ 1000- 1000000")]
+        public int Price { get; set; }
+    }
+}
